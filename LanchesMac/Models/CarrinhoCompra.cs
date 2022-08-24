e52c7e978bf6a3f1b62686a1099246c6ddb49720
Anderson Aires <98119471+andersonaires-cc@users.noreply.github.com>
2022-08-24 19:04:01 -0300

    Aula 47 Concluindo o Carrinho de Compras

--- LanchesMac/Models/CarrinhoCompra.cs
+++ LanchesMac/Models/CarrinhoCompra.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace LanchesMac.Models
 {
@@ -30,6 +31,87 @@ namespace LanchesMac.Models
             {
                 CarrinhoCompraId = carrinhoId
             };
+        }
+
+        public  void AdicionarAoCarrinho(Lanche lanche)
+        {
+            // instância do contexto acessando a tabela CarrinhoCompraItens
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+                // Existe o lancheId
+                s => s.Lanche.LancheId == lanche.LancheId &&
+                    s.CarrinhoCompraId == CarrinhoCompraId);
+            //Lanche não existe no carrinho
+            if(carrinhoCompraItem == null)
+            {
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    // atribuir o Id
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantidade = 1
+                };
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+            }
+            else
+            {
+                // lanche já existe noc arrinho
+                carrinhoCompraItem.Quantidade++;
+            }
+            //persistir no banco de dados
+            _context.SaveChanges();
+        }
+
+        public int RemoveDoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+            s => s.Lanche.LancheId == lanche.LancheId &&
+                s.CarrinhoCompraId == CarrinhoCompraId);
+            var quantidadeLocal = 0;
+
+            if (carrinhoCompraItem != null)
+            {
+                if(carrinhoCompraItem.Quantidade > 1)
+                {
+                    carrinhoCompraItem.Quantidade--;
+                    quantidadeLocal = carrinhoCompraItem.Quantidade;
+                }
+                else
+                {
+                    _context.CarrinhoCompraItens.Remove(carrinhoCompraItem);
+                }
+            }
+            _context.SaveChanges();
+            return quantidadeLocal;
+        }
+
+        public List<CarrinhoCompraItem> GetCarrinhoCompraItems()
+        {
+            // Se for null vai na tabela
+            //CarrinhoCompraItens
+            return CarrinhoCompraItems ??
+                (CarrinhoCompraItems = 
+                _context.CarrinhoCompraItens
+                .Where(c => c.CarrinhoCompraId == CarrinhoCompraId)
+                .Include(s => s.Lanche)
+                .ToList());
+        }
+
+        // método limpar Carrinho
+        public void LimparCarrinho()
+        {
+            var carrinhoItens = _context.CarrinhoCompraItens
+                .Where(Carrinho => Carrinho.CarrinhoCompraId == CarrinhoCompraId);
+
+            _context.CarrinhoCompraItens.RemoveRange(carrinhoItens);
+            _context.SaveChanges();
+        }
+
+        public decimal GetCarrinhoCompraTotal()
+        {
+            var total = _context.CarrinhoCompraItens
+                .Where(c => c.CarrinhoCompraId == CarrinhoCompraId)
+                .Select(c => c.Lanche.Preco * c.Quantidade).Sum();
+            return total;
         }
     }
 }
