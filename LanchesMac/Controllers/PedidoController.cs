6eaded1a8e2f930e889c439955458786f6422027
Anderson Aires <98119471+andersonaires-cc@users.noreply.github.com>
2022-10-10 23:04:37 -0300

    Aula 67 Cirando o método Action Checkout (Httppost)

--- LanchesMac/Controllers/PedidoController.cs
+++ LanchesMac/Controllers/PedidoController.cs
@@ -27,7 +27,45 @@ namespace LanchesMac.Controllers
         [HttpPost]
         public IActionResult Checkout( Pedido pedido)
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            // obter os itens do carrinho de compra do cliente
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItems();
+            _carrinhoCompra.CarrinhoCompraItems = items;
+
+            //verifica se existem itens de pedidos
+            if (_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "seu carrinho esta vazio que tal incluir um lanche...");
+            }
+            // calcular o total de itens e o total do pedido
+            foreach(var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+            // atribuir os valores ao pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+            //validar os dados do pedido
+            if (ModelState.IsValid)
+            {
+                // criar o pedido e os detalhes
+                _pedidoRepository.CriarPedido(pedido);
+
+                // define mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "obrigado pelo seu pedido :)";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //limpar o carrinho do cliente
+                _carrinhoCompra.LimparCarrinho();
+
+                //exibir a view com dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml",pedido);
+
+            }
+            return View(pedido);
         }
     }
 }
