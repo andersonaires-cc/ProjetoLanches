b9b03cc2581ef56a428b71119ab1641016740741
Anderson Aires <98119471+andersonaires-cc@users.noreply.github.com>
2022-11-05 22:31:47 -0300

    Aula 104 Criando a View VendasSemanal para gerar o gráfico de linha

--- LanchesMac/Models/LancheGrafico.cs
+++ LanchesMac/Models/LancheGrafico.cs
@@ -2,7 +2,7 @@
 {
     public class LancheGrafico
     {
-        public string LanchesNome { get; set; }
+        public string LancheNome { get; set; }
         public int LanchesQuantidade { get; set; }
         public decimal LanchesValorTotal { get; set; }
     }
