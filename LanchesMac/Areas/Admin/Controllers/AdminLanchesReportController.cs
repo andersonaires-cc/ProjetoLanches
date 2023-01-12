eeeb8281839c01dd107f77cc6bb9b90132eba9d1
Anderson Aires <98119471+andersonaires-cc@users.noreply.github.com>
2023-01-12 11:46:42 -0300

    Aula 113 Gerando o relatório no formato PDF

--- LanchesMac/Areas/Admin/Controllers/AdminLanchesReportController.cs
+++ LanchesMac/Areas/Admin/Controllers/AdminLanchesReportController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using FastReport.Web;
 using FastReport.Data;
 using LanchesMac.Areas.Admin.FastReportUtils;
+using FastReport.Export.PdfSimple;
 
 namespace LanchesMac.Areas.Admin.Controllers;
 
@@ -28,7 +29,7 @@ public class AdminLanchesReportController : Controller
 
         //Carregar o relatório
         //ContentPath obtém o caminho da pasta raiz que contém todos os arquivos da aplicação
-        webReport.Report.Load(Path.Combine(_webHostEnv.ContentRootPath,"wwwroot/reports", 
+        webReport.Report.Load(Path.Combine(_webHostEnv.ContentRootPath, "wwwroot/reports",
                                             "lanchesCategoria.frx"));
 
 
@@ -40,5 +41,38 @@ public class AdminLanchesReportController : Controller
         webReport.Report.RegisterData(categorias, "CategoriasReport");
 
         return View(webReport);
+    }
+
+    [Route("LanchesCategoriasPDF")]
+    public async Task<ActionResult> LanchesCategoriaPDF()
+    {
+        var webReport = new WebReport();
+        var mssqlDataConnection = new MsSqlDataConnection();
+
+        webReport.Report.Dictionary.AddChild(mssqlDataConnection);
+
+        webReport.Report.Load(Path.Combine(_webHostEnv.ContentRootPath, "wwwroot/reports",
+                                            "lanchesCategoria.frx"));
+
+
+        var lanches = HelperFastReport.GetTable(await _relatorioLanchesService.GetLanchesReport(), "LanchesReport");
+        var categorias = HelperFastReport.GetTable(await _relatorioLanchesService.GetCategoriasReport(), "CategoriasReport");
+
+        webReport.Report.RegisterData(lanches, "LanchesReport");
+        webReport.Report.RegisterData(categorias, "CategoriasReport");
+
+        webReport.Report.Prepare();
+
+        Stream stream = new MemoryStream();
+
+        webReport.Report.Export(new PDFSimpleExport(), stream);
+        stream.Position = 0;
+
+        //fazer donwload PDF
+        //return File(stream, "application/zip", "LancheCategoria.pdf");
+
+        //abrir o PDF no navegador
+        return new FileStreamResult(stream,"application/pdf");
+
     }
 }
